20b4d91df2f2bb277e578effb38b5a7dc38b8548
Jade <jade.applegate@gmail.com>
2014-04-23 20:06:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroceryList::Application.config.secret_key_base = '1c48f15eb05375b090f4d08d3834993a9d5d99962fba9d37dffbf9944a47d5219ae7cf9fc80699c7fbee1f55b95102c5888cbd9ecae54b066818ff9489c5d4fd'
